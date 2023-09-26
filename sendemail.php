719513eff0c249a0dd90ec94f02b27d2dba32a7a
MuhammadNuzhan <131691181+MuhammadNuzhan@users.noreply.github.com>
2023-09-26 17:02:46 +0800

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -1,40 +1,49 @@
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-
-
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
-
-if (isset($_POST["send"])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Load autoloader and create a PHPMailer instance
+    require 'vendor/autoload.php';
     $mail = new PHPMailer(true);
 
+    // Set up SMTP configuration
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'muhammadnuzhan27@gmail.com';
-    $mail->Password = '@nuzhan27';
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port = 465;
+    $mail->Username = 'muhammadnuzhan130505@gmail.com';
+    $mail->Password = 'holv qrzv tjns iqxx';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
 
-    $mail->setFrom('muhammadnuzhan27@gmail.com');
+    // Ambil data dari formulir
+    $nama = $_POST["nama"];
+    $email_pengirim = $_POST["email"];
+    $subjek = $_POST["subjek"];
+    $pesan = $_POST["pesan"];
 
-    $mail->addAddress($_POST["email"]);
+    // Set email content
+    $mail->isHTML(false);
+    $mail->Subject = $subjek;
+    $mail->addReplyTo($email_pengirim, $nama);
+    $mail->Body = "Email: $email_pengirim\n";
+    $mail->Body = "Pesan:\n$pesan";
 
-    $mail->isHTML(true);
+    $tujuan = 'muhammadnuzhan27@gmail.com';
 
-    $mail->Subject = $_POST["subject"];
-    $mail->Body = $_POST["message"];
+    // Set email sender and recipient
+    $mail->setFrom($email_pengirim, $nama);
+    $mail->addAddress($tujuan);
 
-    $mail->send();
-
-    echo "
-    <script>
-    alert('Sent Successfully');
-    document.location.href = 'index.php';
-    </script>
-    ";
+    try {
+        // Send the email
+        $mail->send();
+        echo "Pesan berhasil terkirim";
+        header("Location: index.php#contact");
+        exit();
+    } catch (Exception $e) {
+        echo "Pesan gagal terkirim. Pesan error: {$mail->ErrorInfo}";
+    }
 }
